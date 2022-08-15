c14e4648399ba1b9f32f2388ba6acc8d01ecf606
Ilana Mercer <ilana@pinwheelapi.com>
2022-08-15 19:25:48 -0400

    Problems for dict practice

---
+++ problems.py
@@ -0,0 +1,30 @@
+import json
+
+shows = open('shows-data.json')
+
+show_data = json.load(shows)
+
+# Problem 1:
+# Print each dictionary where the show type is "Play"
+
+
+# Problem 2:
+# print the opening date of each show
+
+
+# Problem 3:
+# print the title of each show where the year is between 1750 and 1755, inclusive
+
+
+# Problem 4:
+# print all show titles and theaters if the show opened in the month of August
+
+
+# Problem 5:
+# print the show titles and their show id if they have "Other titles"
+# if the show doesn't have an other title, print "No Other Titles"
+
+
+# Problem 6:
+# print all theater ids for shows that are Original Productions
+# hint: all ids are four numbers 
